bed37d5e136bdd31f5ff8865a67cde7eeb8a9572
Josué Yeray Julián Ferreiro <josueyeray@gmail.com>
2018-10-23 13:09:05 +0100

    Added service interface and implementation classes

---
+++ XFAlarms/XFAlarms/XFAlarms.Android/Services/AlarmService.cs
@@ -0,0 +1,23 @@
+﻿namespace XFAlarms.Droid.Services
+{
+    using System;
+    using XFAlarms.Services;
+
+    public class AlarmService : IAlarmService
+    {
+        public bool CheckIfAlarmAlreadyExist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateAlarm(string title, string description, DateTime time, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAlarm(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XFAlarms/XFAlarms/XFAlarms/Services/IAlarmService.cs
@@ -0,0 +1,13 @@
+﻿namespace XFAlarms.Services
+{
+    using System;
+
+    public interface IAlarmService
+    {
+        bool CheckIfAlarmAlreadyExist();
+
+        bool CreateAlarm(string title, string description, DateTime time, int id);
+
+        bool DeleteAlarm(int id);
+    }
+}
