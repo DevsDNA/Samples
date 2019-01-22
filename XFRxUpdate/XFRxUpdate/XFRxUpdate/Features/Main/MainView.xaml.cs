4480a41c3ebbe15e9b302c158397bbdab52a628d
Ciani Afonso Díaz <cafonso@devsdna.com>
2019-01-22 12:24:28 +0000

    Added new project XFRxUpdate

---
+++ XFRxUpdate/XFRxUpdate/XFRxUpdate/Features/Main/MainView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace XFRxUpdate.Features.Main
+{
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XFRxUpdate/XFRxUpdate/XFRxUpdate/Features/Main/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace XFRxUpdate.Features.Main
+{
+    using ReactiveUI;
+
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
