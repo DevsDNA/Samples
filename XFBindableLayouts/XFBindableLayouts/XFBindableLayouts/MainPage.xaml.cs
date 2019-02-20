8017f140e554ceb61c7d8f4e0433b4877429b32d
Josué Yeray Julián Ferreiro <josueyeray@gmail.com>
2019-02-20 12:44:30 +0000

    Sample for bindable layout video

---
+++ XFBindableLayouts/XFBindableLayouts/XFBindableLayouts/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XFBindableLayouts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Users = new List<UserModel>();
+            for (int i = 0; i < 15; i++)
+            {
+                Users.Add(new UserModel() { Name = $"User #{i}" });
+            }
+
+            BindingContext = this;
+        }
+
+        public List<UserModel> Users
+        {
+            get;
+            set;
+        }
+    }
+
+    public class UserModel
+    {
+        public string Name { get; set; }
+    }
+}
