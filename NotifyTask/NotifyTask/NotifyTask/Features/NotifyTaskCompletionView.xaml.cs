37060941f97816a7d2bed67aeaa10ec2138106f3
Enaut Manterola <enaut@devsdna.com>
2019-03-13 11:15:11 +0100

    Added sample for 13/03/2019 pill

---
+++ NotifyTask/NotifyTask/NotifyTask/Features/NotifyTaskCompletionView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace NotifyTask.Features
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotifyTaskCompletionView : ContentPage
+	{
+		public NotifyTaskCompletionView ()
+		{
+			InitializeComponent ();
+            BindingContext = new NotifyTaskCompletionViewModel();
+		}
+
+	}
+}
---
+++ NotifyTask/NotifyTask/NotifyTask/Features/NotifyTaskCompletionViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace NotifyTask.Features
+{
+    using NotifyTask.Services;
+    public class NotifyTaskCompletionViewModel
+    {
+        public NotifyTaskCompletionViewModel()
+        {
+            UrlByteCount = new NotifyTaskCompletion<int>(
+              MyStaticService.CountBytesInUrlAsync("https://www.google.com"));
+        }
+        public NotifyTaskCompletion<int> UrlByteCount { get; private set; }
+    }
+}
