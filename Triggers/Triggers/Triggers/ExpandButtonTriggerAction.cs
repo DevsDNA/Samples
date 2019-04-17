ddb0b63a7b64eb23212235c3aa7cf57952e18753
jorgediegocrespo <jorgediegocrespo@gmail.com>
2019-04-17 11:07:26 +0200

    Triggers sample

---
+++ Triggers/Triggers/Triggers/ExpandButtonTriggerAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Triggers
+{
+    using Xamarin.Forms;
+
+    public class ExpandButtonTriggerAction : TriggerAction<Button>
+    {
+        protected override async void Invoke(Button sender)
+        {
+            await sender.ScaleTo(0.95, 50, Easing.CubicOut);
+            await sender.ScaleTo(1, 50, Easing.CubicIn);
+        }
+    }
+}
