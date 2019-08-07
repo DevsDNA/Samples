a31455c495c1097aa5f525ac37d653c07816f83c
Jorge DevsDNA <jorge@devsdna.com>
2019-08-07 15:14:44 +0200

    Javascript injector

---
+++ JavascriptInjection/JavascriptInjector/JavascriptInjector/JavascriptInjector/CustomWebView.cs
@@ -0,0 +1,19 @@
+﻿namespace JavascriptInjector
+{
+    using System;
+    using Xamarin.Forms;
+
+    public class CustomWebView : WebView
+    {
+        public Action<string> SetSearchText;
+        public Action DoSearch;
+        public Action Observe;
+
+        public event EventHandler SearchPreseed;
+
+        public void OnSearchBtnPressed()
+        {
+            SearchPreseed?.Invoke(this, null);
+        }
+    }
+}
---
+++ JavascriptInjection/JavascriptInjector/JavascriptInjector/JavascriptInjector/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JavascriptInjector
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Cwv_SearchPreseed(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "Button pressed", "OK");
+        }
+
+        private void BtnText_Clicked(object sender, EventArgs e)
+        {
+            cwv.SetSearchText("DevsDNA");
+        }
+
+        private void BtnSearch_Clicked(object sender, EventArgs e)
+        {
+            cwv.DoSearch();
+        }
+
+        private void BtnObserve_Clicked(object sender, EventArgs e)
+        {
+            cwv.Observe();
+        }
+    }
+}
