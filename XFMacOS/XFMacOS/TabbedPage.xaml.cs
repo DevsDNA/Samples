c4ea860c1a569c7d70ac97fd434df22da19417c3
Luis Marcos Rivera <lmarcos@devsdna.com>
2021-03-03 12:41:50 +0100

    Added XF macOS example

---
+++ XFMacOS/XFMacOS/TabbedPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace XFMacOS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage : Xamarin.Forms.TabbedPage
+    {
+        public TabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
