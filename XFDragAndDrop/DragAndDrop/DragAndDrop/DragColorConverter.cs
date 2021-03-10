31c12f609ea22fbf6ca2a3760b659b1e1a308614
Marcos Antonio Blanco <mablanco@devsdna.com>
2021-03-10 09:35:08 +0100

    Added drag and drop in collection view sample.

---
+++ XFDragAndDrop/DragAndDrop/DragAndDrop/DragColorConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace DragAndDrop
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+
+
+    public class DragColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isBeingDragged = (bool?)value;
+            var result = (isBeingDragged ?? false) ? Color.LightGray : Color.Azure;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XFDragAndDrop/DragAndDrop/DragAndDrop/ItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace DragAndDrop
+{
+    using Xamarin.Forms;
+
+    public class ItemViewModel : BindableObject
+    {
+        private bool isBeingDragged;
+        private bool isBeingDraggedOver;
+        private string category;
+        private string title;
+
+        public string Category
+        {
+            get => category;
+            set { category = value; OnPropertyChanged(); }
+        }
+
+        public string Title
+        {
+            get => title;
+            set { title = value; OnPropertyChanged(); }
+        }
+
+        public bool IsBeingDragged
+        {
+            get => isBeingDragged;
+            set { isBeingDragged = value; OnPropertyChanged(); }
+        }
+
+        public bool IsBeingDraggedOver
+        {
+            get => isBeingDraggedOver;
+            set { isBeingDraggedOver = value; OnPropertyChanged(); }
+        }
+    }
+}
