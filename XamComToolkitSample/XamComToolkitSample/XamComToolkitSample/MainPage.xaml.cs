bfe702e2bda560e4faadab733222d8804628841e
Yeray <jyeray@devsdna.com>
2021-03-24 20:09:33 +0000

    XCT sample

---
+++ XamComToolkitSample/XamComToolkitSample/XamComToolkitSample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace XamComToolkitSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.CommunityToolkit.Effects;
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SafeAreaEffect.SetSafeArea(root, new Xamarin.CommunityToolkit.Helpers.SafeArea(true));
+        }
+
+        private void CameraView_MediaCaptured(object sender, Xamarin.CommunityToolkit.UI.Views.MediaCapturedEventArgs e)
+        {
+
+        }
+    }
+}
