ea2a1e3a1b537b93b6632c710793a44e2721d689
Ciani <cafonso@devsdna.com>
2021-04-21 13:57:07 +0100

    Added WorkManager sample

---
+++ Workers/Workers/Workers/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿namespace Workers
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Runtime;
+    using AndroidX.AppCompat.App;
+    using AndroidX.Work;
+    using global::Workers.Workers;
+    using System;
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            var periodicWorkRequest = 
+                new PeriodicWorkRequest.Builder(typeof(MyListenableWorker)
+                ,TimeSpan.FromSeconds(60),TimeSpan.FromSeconds(10))
+                .AddTag("MyListenableWorker")
+                .Build();
+
+            WorkManager.GetInstance(Application.Context).Enqueue(periodicWorkRequest);
+
+            var oneTimeWorkRequest = new OneTimeWorkRequest.Builder(typeof(MyWorker))
+                .AddTag("MyListenableWorker")
+                .SetInitialDelay(TimeSpan.FromSeconds(10))
+                .Build();
+
+            WorkManager.GetInstance(Application.Context)
+                .BeginUniqueWork("MyListenableWorker", ExistingWorkPolicy.Keep
+                , oneTimeWorkRequest).Enqueue();
+             
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Workers/Workers/Workers/Workers/MyListenableWorker.cs
@@ -0,0 +1,31 @@
+﻿namespace Workers.Workers
+{
+    using Android.Content;
+    using AndroidX.Concurrent.Futures;
+    using AndroidX.Work;
+    using Google.Common.Util.Concurrent;
+    using System;
+    using System.Threading.Tasks;
+
+    public class MyListenableWorker : ListenableWorker, CallbackToFutureAdapter.IResolver
+    {
+        public MyListenableWorker(Context appContext, WorkerParameters workerParams) : base(appContext, workerParams)
+        {
+        }
+
+        public Java.Lang.Object AttachCompleter(CallbackToFutureAdapter.Completer p0)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                return p0.Set(Result.InvokeSuccess());
+            });
+            return "MyListenableWorker";
+        }
+
+        public override IListenableFuture StartWork()
+        {
+            return CallbackToFutureAdapter.GetFuture(this);
+        }
+    }
+}
