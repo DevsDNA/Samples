8740a3a29736cc7b1f45ac41a975042eca41cd8d
Yeray <jyeray@devsdna.com>
2021-06-10 11:47:10 +0100

    swipe sample

---
+++ XFSwipe/XFSwipe/XFSwipe/XFSwipe/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace XFSwipe
+{
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            deleteSwipe.Command = new Command(() =>
+            {
+                DisplayAlert("borrar", "¿Está seguro de borrar este elemento?", "aceptar", "cancelar");
+            });
+            editSwipe.Command = new Command(() =>
+            {
+                lblText.IsVisible = !lblText.IsVisible;
+                txtEdit.IsVisible = !txtEdit.IsVisible;
+                editSwipe.Text = txtEdit.IsVisible ? "finalizar" : "editar";
+            });
+            favouriteSwipe.Command = new Command(() =>
+            {
+                shapeFavourite.IsVisible = !shapeFavourite.IsVisible;
+                favouriteSwipe.Text = shapeFavourite.IsVisible ? "desmarcar favorito" : "marcar favorito";
+            });
+        }
+    }
+}
