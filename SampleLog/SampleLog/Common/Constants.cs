20f274ae63b7ee896d56077a215d24fef2610b71
Marcos Antonio Blanco <mablanco@devsdna.com>
2021-07-21 12:24:58 +0200

    Added missing file.

---
+++ SampleLog/SampleLog/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleLog.Common
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	public static class Constants
+	{
+		public const string AppCenterKey = "android={Your Android App secret here};uwp={Your UWP App secret here};ios={Your iOS App secret here}";
+	}
+}
