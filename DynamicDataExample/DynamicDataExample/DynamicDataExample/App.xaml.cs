45df9d8fdc7bc931e384d880750830a4ccf67e86
Jorge Diego <jorge@devsdna.com>
2021-10-14 13:27:45 +0200

    Dynamic data example

---
+++ DynamicDataExample/DynamicDataExample/DynamicDataExample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicDataExample
+{
+    using DynamicDataExample.Features;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new DynamicDataView() { ViewModel = new DynamicDataViewModel() };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DynamicDataExample/DynamicDataExample/DynamicDataExample/Features/DynamicDataView.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace DynamicDataExample.Features
+{
+    using ReactiveUI;
+    using System.Reactive.Disposables;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DynamicDataView
+    {
+        public DynamicDataView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void CreateBindings(CompositeDisposable disposables)
+        {
+            base.CreateBindings(disposables);
+
+            disposables.Add(this.Bind(ViewModel, vm => vm.SearchText, v => v.enSearch.Text));
+
+            disposables.Add(this.OneWayBind(ViewModel, vm => vm.SortByList, v => v.pkSort.ItemsSource));
+            disposables.Add(this.Bind(ViewModel, vm => vm.SortBy, v => v.pkSort.SelectedItem));
+
+            disposables.Add(this.OneWayBind(ViewModel, vm => vm.ItemList, v => v.cvAll.ItemsSource));
+            disposables.Add(this.OneWayBind(ViewModel, vm => vm.LoadMoreItemListCommand, v => v.cvAll.RemainingItemsThresholdReachedCommand));
+        }
+    }
+}
