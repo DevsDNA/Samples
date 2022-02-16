825b8f6fa78a64f5b6a5771379d3236c50bfdec5
LuisM000 <luis.m.r@outlook.com>
2022-02-16 17:16:35 +0100

    Added logic to TaskListController

--- ToDo/Todo.WebAPI/Controllers/TaskListController.cs
+++ ToDo/Todo.WebAPI/Controllers/TaskListController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Todo.Common.Models.Requests;
 using Todo.Common.Models.Responses;
 using Todo.WebAPI.Database;
+using Todo.WebAPI.ModelExtension;
+using Todo.WebAPI.Models;
 
 namespace Todo.WebAPI.Controllers
 {
@@ -18,20 +20,30 @@ namespace Todo.WebAPI.Controllers
         }
 
         [HttpGet]
-        [Route("items/{id:int:min(1)}")]
+        [Route("items/{id:int:min(1)}", Name = nameof(GetTaskListByIdAsync))]
         [ProducesResponseType(typeof(TaskListDetailResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<TaskListDetailResponse>> GetTaskListByIdAsync(int id)
         {
-            return Ok();
+            var taskList = await dbContext.TaskLists.Include(tl => tl.Tasks)
+                                                    .FirstOrDefaultAsync(tl => tl.Id == id);
+
+            if (taskList == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(taskList.ToTaskListDetailResponse());
         }
 
         [HttpGet]
         [Route("items")]
         [ProducesResponseType(typeof(TaskListResponse), StatusCodes.Status200OK)]
-        public async Task<ActionResult<TaskListResponse>> GetTaskListsAsync()
+        public async Task<ActionResult<IEnumerable<TaskListResponse>>> GetTaskListsAsync()
         {
-            return Ok();
+            var tasksList = await dbContext.TaskLists.ToListAsync();
+
+            return Ok(tasksList.Select(tl => tl.ToTaskListResponse()));
         }
 
 
@@ -41,7 +53,17 @@ namespace Todo.WebAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> CreateTaskListAsync([FromBody] CreateTaskListRequest request)
         {
-            return Ok();
+            if(string.IsNullOrEmpty(request.Title))
+            {
+                return BadRequest("Title must not be empty.");
+            }
+
+            var taskList = new TaskList(request.Title);
+
+            await dbContext.TaskLists.AddAsync(taskList);
+            var createdId = await dbContext.SaveChangesAsync();
+
+            return CreatedAtRoute(nameof(GetTaskListByIdAsync), new { id = createdId}, null);
         }
 
         [HttpPut]
@@ -51,6 +73,22 @@ namespace Todo.WebAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> UpdateTaskListAsync(int id, [FromBody] UpdateTaskListRequest request)
         {
+            if (string.IsNullOrEmpty(request.Title))
+            {
+                return BadRequest("Title must not be empty.");
+            }
+
+            var taskList = await dbContext.TaskLists.FindAsync(id);
+            if(taskList == null)
+            {
+                return NotFound();
+            }
+
+            taskList.Title = request.Title;
+
+            dbContext.TaskLists.Update(taskList);
+            await dbContext.SaveChangesAsync();
+
             return NoContent();
         }
 
@@ -60,6 +98,15 @@ namespace Todo.WebAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> DeleteTaskListAsync(int id)
         {
+            var taskList = await dbContext.TaskLists.FindAsync(id);
+            if (taskList == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.TaskLists.Remove(taskList);
+            await dbContext.SaveChangesAsync();
+
             return NoContent();
         }
     }
